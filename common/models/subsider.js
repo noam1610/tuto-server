b14fea6953678786d1d78d86d128649fb870b382
noam1610 <nbenlolo@yoobic.com>
2015-06-24 12:16:55 +0300

    feat(app):ca va marcher cette fois

--- common/models/subsider.js
+++ common/models/subsider.js
@@ -1,13 +1,13 @@
-module.exports = function(Subsider) {
-    Subsider.sharedClass.find('create', true).shared = true;
-    Subsider.sharedClass.find('update', true).shared = false;
-    Subsider.sharedClass.find('upsert', true).shared = false; 
-    Subsider.sharedClass.find('updateAttributes', false).shared = false;
-    Subsider.sharedClass.find('deleteById', true).shared = false;
-    Subsider.sharedClass.find('find', true).shared = false; 
-    Subsider.sharedClass.find('findById', true).shared = false; 
-    Subsider.sharedClass.find('count', true).shared = false; 
-    Subsider.sharedClass.find('exists', true).shared = false; 
-    Subsider.sharedClass.find('findOne', true).shared = false;
+module.exports = function(subsider) {
+    subsider.sharedClass.find('create', true).shared = true;
+    subsider.sharedClass.find('update', true).shared = false;
+    subsider.sharedClass.find('upsert', true).shared = false; 
+    subsider.sharedClass.find('updateAttributes', false).shared = false;
+    subsider.sharedClass.find('deleteById', true).shared = false;
+    subsider.sharedClass.find('find', true).shared = false; 
+    subsider.sharedClass.find('findById', true).shared = false; 
+    subsider.sharedClass.find('count', true).shared = false; 
+    subsider.sharedClass.find('exists', true).shared = false; 
+    subsider.sharedClass.find('findOne', true).shared = false;
 
 };
